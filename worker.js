67e35bcab2eb3b67a5c96a47806c38663e27546c
YoungUsing <67814050+YoungUsing@users.noreply.github.com>
2022-12-03 12:00:46 +0800

    Add if...else

--- worker.js
+++ worker.js
@@ -5,6 +5,11 @@ const root="/"
 const driver_id="b!"
 
 async function handleRequest(request){
+    const requestURL=new URL(request.url)
+    const path=requestURL.pathname
+    if (path==="/"){
+        return new Response("Nothing to request",{status:400})
+    } else{
     let tokenURL=await fetch("https://login.microsoftonline.com/"+tenate+"/oauth2/v2.0/token", {
           method: "POST",
           body: "client_id="+client_id+"&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default&client_secret="+client_secret+"&grant_type=client_credentials",
@@ -14,8 +19,6 @@ async function handleRequest(request){
       })
     token_text=JSON.stringify(await tokenURL.json())
     token='Bearer '+token_text.slice(79,-2)
-    const requestURL=new URL(request.url)
-    const path=requestURL.pathname
     let redirectJSON=await fetch("https://graph.microsoft.com/v1.0/drives/"+driver_id+"/root:"+root+path+":?select=@microsoft.graph.downloadUrl",{
               method:"GET",
               headers: {
@@ -23,6 +26,7 @@ async function handleRequest(request){
           },
         }
     )
+    if (redirectJSON.status=="200"){
     redirectinf=JSON.stringify(await redirectJSON.json())
     redirectplace=redirectinf.indexOf("microsoft.graph.downloadUrl")
     redirectURL=redirectinf.slice(redirectplace+30,-2)
@@ -30,9 +34,13 @@ async function handleRequest(request){
         headers:{
           "Location":redirectURL
       }
-    })
+    })}
+    else{
+        return new  Response("Resource not found",{status:404})
+    }
   }
-  addEventListener("fetch", event => {
+}
+
+addEventListener("fetch", event => {
     event.respondWith(handleRequest(event.request))
-  })
-  
+})
