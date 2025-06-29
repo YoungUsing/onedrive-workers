4edc69fe6a4d5d40eaa3833794f8098471bfa56e
YoungUsing <67814050+YoungUsing@users.noreply.github.com>
2025-06-29 14:49:50 +0800

    Updated worker.js

--- worker.js
+++ worker.js
@@ -2,43 +2,81 @@ const client_id=""
 const client_secret=""
 const tenate=""
 const root="/"
-const driver_id="b!"
+const driver_id=""
+const proxyhost=""
 
 async function handleRequest(request){
     const requestURL=new URL(request.url)
-    const path=requestURL.pathname
+    const queryst=requestURL.searchParams
+    const view=queryst.get("view")
+    const proxystatus=Boolean(queryst.get("proxy"))
+    console.log(requestURL)
+    let path=requestURL.pathname
+    console.log(path)
     if (path==="/"){
         return new Response("Nothing to request",{status:400})
-    } else{
+    } 
+    else{
+        var res=await getlink(path)
+        console.log(res)
+        if (String(res)===String("2")) {
+            return new Response(null,{status:404})
+        } else {
+            if (String(res)===String("1")) {
+                return new Response("An error happened,please contact the server owner for further information.",{status:500})
+            }
+            if (proxystatus==true&&(proxyhost)) {
+                var resurl=new URL(res)
+                resurl.hostname=proxyhost
+                res=resurl
+            }
+            if (view=="video") {
+                return new Response('<html lang="en"><head><title>'+path+'</title></head><body><video src="'+res+'" width="100%" height="100%" controls></video></body></html>',
+                {status:200,
+                    headers:{
+                        "Content-Type":"text/html"
+                    }
+                })
+            }
+            else{
+                return new Response(null,{status:302,
+                    headers:{
+                      "Location":res
+                    }
+                })
+            }
+        }
+    }
+
+}
+
+async function getlink(path) {
     let tokenURL=await fetch("https://login.microsoftonline.com/"+tenate+"/oauth2/v2.0/token", {
-          method: "POST",
-          body: "client_id="+client_id+"&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default&client_secret="+client_secret+"&grant_type=client_credentials",
-          headers: {
-              "Content-Type": "application/x-www-form-urlencoded"
-          }
+        method: "POST",
+        body: "client_id="+client_id+"&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default&client_secret="+client_secret+"&grant_type=client_credentials",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
       })
-    token_text=JSON.stringify(await tokenURL.json())
-    token='Bearer '+token_text.slice(79,-2)
+    let tokenjson=await tokenURL.json()
+    let token='Bearer '+tokenjson.access_token
     let redirectJSON=await fetch("https://graph.microsoft.com/v1.0/drives/"+driver_id+"/root:"+root+path+":?select=@microsoft.graph.downloadUrl",{
-              method:"GET",
-              headers: {
-              "Authorization": token
-          },
+        method:"GET",
+        headers: {"Authorization": token},
         }
     )
-    if (redirectJSON.status=="200"){
-    redirectinf=JSON.stringify(await redirectJSON.json())
-    redirectplace=redirectinf.indexOf("microsoft.graph.downloadUrl")
-    redirectURL=redirectinf.slice(redirectplace+30,-2)
-    return new Response(null,{status:302,
-        headers:{
-          "Location":redirectURL
-      }
-    })}
+    if (redirectJSON.status==Number("200")){
+        let redirectinf=await redirectJSON.json()
+        let redirectURL=redirectinf['@microsoft.graph.downloadUrl']
+        return redirectURL
+    }       
     else{
-        return new  Response("Resource not found",{status:404})
+        if (redirectJSON.status==Number("404")) {
+            return "2"
+        } else {
+            return "1"
+        }
     }
-  }
 }
 
 addEventListener("fetch", event => {
